2d6f05db78e45fcf7388f44fa13eac108dc316ab
Scuttsy <scutts001@gmail.com>
2025-12-30 19:42:20 +0100

    feat: Updated ObjectiveCompleted Module
    Added ObjectiveId and Objective to the definition

--- SFWebservice/Modules/ObjectiveCompleted.cs
+++ SFWebservice/Modules/ObjectiveCompleted.cs
@@ -9,7 +9,11 @@ public partial class ObjectiveCompleted
 
     public int? GameSessionId { get; set; }
 
+    public int? ObjectiveId { get; set; }
+
     public DateTime? TimeCompleted { get; set; }
+
+    public virtual Objective? Objective { get; set; }
 
     public virtual GameSession? GameSession { get; set; }
 }
